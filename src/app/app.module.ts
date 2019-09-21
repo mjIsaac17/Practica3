e82f437929f55790cd9dfdbe7b3e2bfc8d81d16a
Isaac <isaac1998188@gmail.com>
2019-09-20 21:01:20 -0700

    se ha agregado un navbar con menu lateral donde redirecciona a otras paginas en blanco, tambien los componentes de dichas paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router'; 
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { 
+  MatToolbarModule, 
+  MatButtonModule, 
+  MatSidenavModule, 
+  MatIconModule, 
+  MatListModule ,
+  MatCardModule,
+} from '@angular/material';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { VideojuegosComponent } from './components/videojuegos/videojuegos.component';
+import { FitnessComponent } from './components/fitness/fitness.component';
+
+const appRoutes: Routes = [
+  {path: 'videojuegos', component: VideojuegosComponent},
+  {path: 'fitness', component: FitnessComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    NavBarComponent,
+    VideojuegosComponent,
+    FitnessComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
@@ -19,7 +42,8 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/videojuegos/videojuegos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-videojuegos',
+  templateUrl: './videojuegos.component.html',
+  styleUrls: ['./videojuegos.component.css']
+})
+export class VideojuegosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
